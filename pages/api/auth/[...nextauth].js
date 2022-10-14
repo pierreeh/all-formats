7ffe336d338e293f432eebc54980e14eeca39127
pierre018 <pierreeh@gmail.com>
2022-10-14 17:45:58 +0200

    auth, hide fb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -20,10 +20,10 @@ export default async function Auth(req, res) {
           clientId: process.env.GOOGLE_CLIENT_ID,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
-        FacebookProvider({
-          clientId: process.env.FACEBOOK_CLIENT_ID,
-          clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-        })
+        // FacebookProvider({
+        //   clientId: process.env.FACEBOOK_CLIENT_ID,
+        //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+        // })
       ],
       pages: {
         signIn: '/auth/signin',
